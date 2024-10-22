6b7ed6731c0b06471e9ec32fccd1e170904e6ca1
Abdirazak-Qoryooley <justarday33@gmail.com>
2024-10-21 20:51:10 -0700

    responsive

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_design/home/home_view.dart';
+
+void main() => runApp(const ResponsiveDesignApp());
+
+class ResponsiveDesignApp extends StatelessWidget {
+  const ResponsiveDesignApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Responsive UI', home: HomeView());
+  }
+}
